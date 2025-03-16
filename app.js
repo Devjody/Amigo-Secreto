74823381440d12fa025abe7e0b74740be459acd9
Devjody <wellingtonjohnnydossantos@gmail.com>
2025-03-16 16:39:05 -0300

    Implementando ação do botão 'Adicionar'.

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaDeAmigosSorteados = [];
 
+function adicionarAmigo() {
+
+    let ul = document.getElementById("listaAmigos");
+    let input = document.getElementById("amigo");
+    listaDeAmigosSorteados.push(document.getElementById("amigo").value);
+    
+    if (input.value.trim() !== "") { // Verifica se o campo não está vazio
+        let li = document.createElement("li");
+        li.textContent = input.value;
+        ul.appendChild(li);
+    }else{
+        alert("Insira um texto!");
+    }
+    console.log(listaDeAmigosSorteados);
+    limparCampo();
+}
+
+function limparCampo(){
+    limpar = document.querySelector('input');
+    limpar.value = '';
+}
+
